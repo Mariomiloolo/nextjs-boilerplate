cc68da376157489d26022efba716e6b30bd6ef02
Mariomiloolo <156296113+Mariomiloolo@users.noreply.github.com>
2025-07-15 22:20:34 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-export default function Home () {
-	return (
-		<h1>PropertyFlow - LIVE system!</h1>
-			)
-				}
+export default function Home() {
+return (
+<div style={{background: 'blue', height: '100vh', padding: '50px'}}>
+<h1 style={{color: 'white', textAlign: 'center'}}>PropertyFlow LIVE</h1>
+</div>
+)
